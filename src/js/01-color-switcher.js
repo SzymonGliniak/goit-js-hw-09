bdeaea620c43da746239e1ab300ad878bd1d42f4
SzymonGliniak <mactare@gmail.com>
2023-12-17 15:49:02 +0100

    skonczone

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
 
+    const body = document.querySelector("body");
+     const buttonStart = document.querySelector("button[data-start]");
+     const buttonStop = document.querySelector("button[data-stop]");
+
+    let nIntervId;
+    buttonStop.disabled = true;
+
+function changeColor() {
+  if (!nIntervId) {
+    nIntervId = setInterval(flashBody, 1000);
+   
+  }
+}
+
+function flashBody() {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function stopColor() {
+  clearInterval(nIntervId);
+  buttonStop.disabled = true;
+  buttonStart.disabled = false;
+  nIntervId = null
+}
+
+buttonStart.addEventListener("click", changeColor);
+buttonStop.addEventListener("click", stopColor);
